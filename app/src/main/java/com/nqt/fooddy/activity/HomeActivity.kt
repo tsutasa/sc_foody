b83fb654fd85b4cdfab0937e6c210ab3bc64c70d
tsutasa <hoa.nhh97hd@gmail.com>
2020-11-09 22:41:19 +0700

    part 2

--- app/src/main/java/com/nqt/fooddy/activity/HomeActivity.kt
+++ app/src/main/java/com/nqt/fooddy/activity/HomeActivity.kt
@@ -1,13 +1,17 @@
 package com.nqt.fooddy.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.WindowManager
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.nqt.fooddy.R
+import com.nqt.fooddy.fragment.*
 import kotlinx.android.synthetic.main.activity_home.*
+
 
 @Suppress("DEPRECATION")
 class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -20,28 +24,45 @@ class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     private fun initView() {
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        toolbar = supportActionBar!!
+//        toolbar = supportActionBar!!
         nav.setOnNavigationItemSelectedListener(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.navHome -> {
+                val fragment = HomeFragment()
+                loadFragment(fragment)
                 return true
             }
             R.id.navLove -> {
+                val fragment = FavouriteFragment()
+                loadFragment(fragment)
                 return true
             }
             R.id.navBuy -> {
+                val fragment = CartFragment()
+                loadFragment(fragment)
                 return true
             }
             R.id.navNotification -> {
+                val fragment = NotificationFragment()
+                loadFragment(fragment)
                 return true
             }
             R.id.navProfile -> {
+                val fragment = ProfileFragment()
+                loadFragment(fragment)
                 return true
             }
         }
         return false
+    }
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/nqt/fooddy/activity/LoginActivity.kt
+++ app/src/main/java/com/nqt/fooddy/activity/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.nqt.fooddy.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nqt.fooddy.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
     }
 }
